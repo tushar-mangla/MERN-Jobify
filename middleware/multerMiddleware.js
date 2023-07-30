61a78c05924cbd3737cd1724926d8e4b60fb238b
Tushar Mangla <tushar.mangla1120@gmail.com>
2023-07-30 18:31:44 +0530

    fix update user

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -9,7 +9,7 @@ const parser = new DataParser();
 
 export const formatImage = (file) => {
   const fileExtension = path.extname(file.originalname).toString();
-  return parser.format(fileExtension, file.buffer).content();
+  return parser.format(fileExtension, file.buffer).content;
 };
 
 export default upload;
